943340940d9ede288ec5eacee381572ee4b96896
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-05-12 16:21:59 -0400

    Update Experiences.js

--- src/components/pages/experiences/Experiences.js
+++ src/components/pages/experiences/Experiences.js
@@ -32,21 +32,44 @@ export default function Experiences() {
           <div className="row mt-top">
             <div className="col xl4 l4 m6 s12">
               <p className="teal year_exp white-text">
-                Jan
-                <strong>2016</strong> - Mar
-                <strong>2017</strong>
+                Feb
+                <strong>2018</strong> - Mar
+                <strong>2019</strong>
               </p>
             </div>
             <div className="col xl8 l8 m6 s12">
               <blockquote className="no-pad">
                 <h6 className="no-pad mt-bottom">
-                  <strong>WEB DEVELOPER</strong>
+                  <strong>IT TECHNICAL SUPPORT SPECIALIST</strong>
                 </h6>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
-                  earum doloribus dicta quod architecto, praesentium totam
-                  molestiae similique culpa repellat blanditiis nam facilis eius
-                  quaerat distinctio vitae, tenetur beatae repellendus?
+                •	Configure and install new employee workstations across all departments.<br/>
+                •	Maintain 90% accuracy on ticket escalation.<br/>
+                •	Configure and maintain Active Directory and GPO.<br/>
+
+                </p>
+              </blockquote>
+            </div>
+          </div>
+          <div className="row mt-top">
+            <div className="col xl4 l4 m6 s12">
+              <p className="teal year_exp white-text">
+                Sep
+                <strong>2019</strong> - Present
+               
+              </p>
+            </div>
+            <div className="col xl8 l8 m6 s12">
+              <blockquote className="no-pad">
+                <h6 className="no-pad mt-bottom">
+                  <strong>TECHNICAL SUPPORT SPECIALIST</strong>
+                </h6>
+                <p>
+                •	Maintain 90% accuracy on ticket escalations across core service areas.<br/>
+                •	Assist in Co-pilot projects and Tier 2 escalations. In technical support.<br/>
+                •	Maintain 90% monthly KPIs on customer satisfaction.<br/>
+                •	Track and document all dispatch transactions in spreadsheets.<br/>
+                •	Providing, verifying, and/or modifying network settings (TCP/IP).<br/>
                 </p>
               </blockquote>
             </div>
