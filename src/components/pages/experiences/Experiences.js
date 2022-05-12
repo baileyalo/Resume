0e571b3b2e4d1382b857d181e5f24c88b93f54f5
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-05-12 16:14:21 -0400

    Update Experiences.js

--- src/components/pages/experiences/Experiences.js
+++ src/components/pages/experiences/Experiences.js
@@ -11,21 +11,20 @@ export default function Experiences() {
           <div className="row mt-top">
             <div className="col xl4 l4 m6 s12">
               <p className="teal year_exp white-text">
-                Jan
-                <strong>2016</strong> - Mar
-                <strong>2017</strong>
+                June
+                <strong>2010</strong> - Dec
+                <strong>2016</strong>
               </p>
             </div>
             <div className="col xl8 l8 m6 s12">
               <blockquote className="no-pad">
                 <h6 className="no-pad mt-bottom">
-                  <strong>WEB DEVELOPER</strong>
+                  <strong>Head Teller</strong>
                 </h6>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
-                  earum doloribus dicta quod architecto, praesentium totam
-                  molestiae similique culpa repellat blanditiis nam facilis eius
-                  quaerat distinctio vitae, tenetur beatae repellendus?
+                •	Maintain 95% in transactions processing.<br/>
+                •	Maintain and manage branch cash liaising with Bank of Jamaica (BOJ) and other departments.<br/>
+                •	Cross-trained as Superuser for core system upgrade and exposed to the software lifecycle.<br/>
                 </p>
               </blockquote>
             </div>
